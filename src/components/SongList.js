acb5171db04f2bc3a2959b8c458add7abeb558b5
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-04-22 21:47:22 +0530

    Consoled useEffect with new updates

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import NewSongForm from "./NewSongForm";
 
@@ -9,9 +9,20 @@ function SongList() {
     { title: "You're Welcome", id: 3 },
   ]);
 
+  const [age, setAge] = useState(20);
+
   const addSong = (title) => {
     setSongs([...songs, { title: title, id: uuidv4() }]);
   };
+
+  // useEffect Hook:
+  // - useEffect hook is bit like a lifecycle method that we normally use in class based components.
+  // - When we use functional component we don't have access to lifecycle methods.
+  // - we can use useEffect to communicate with DB or connect with an API endpoint or something like these.
+  // - this function will run everytime our app re-renders or some data updates.
+  useEffect(() => {
+    console.log("useEffect hook ran", songs);
+  });
 
   return (
     <div className="song-list">
@@ -21,6 +32,8 @@ function SongList() {
         })}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
+      {/* suppose we have another state age which is changed by this button. Then also useEffect will run amd thats bad */}
     </div>
   );
 }
